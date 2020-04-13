7892141dd56904545cadbc7df09c028c0dc1a4d7
“mvinicius112015@gmail.com” <mvinicius112015@gmail.com>
2020-04-12 22:55:16 -0300

    Alterações Finais

--- aula2/src/app/eventos/eventos.component.ts
+++ aula2/src/app/eventos/eventos.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  evento =
-    { name: 'PHP Conference', date: '6/30/2020', time: '10am', location: { address: 'UniBH ', city: 'Belo Horizonte', country: 'Brasil' } }
+  evento = [
+    { name: ' Villa Mix ', date: ' 9/26/2020 ', time: ' 10am ', location: { address: ' Mineirão ', city: ' Belo Horizonte ', country: ' Brasil ' } },
+    { name: ' Rock In Rio ', date: ' 4/15/2020 ', time: ' 9am ', onlineUrl: ' https://www.rockinrio.com.br/ ' },
+    { name: ' Campus Party ', date: ' 6/10/2020 ', time: ' 8am ' },
+    { name: ' Loolapalooza ', date: ' 4/15/2020 ', time: ' 9am ', onlineUrl: ' http://www.loolapalooza.com ' },
+    { name: ' Samba Prime ', date: ' 6/10/2020 ', time: ' 8am ', location: { address: ' Expominas ', city: ' Belo Horizonte ', country: ' Brasil ' }, onlineUrl: ' http://www.sambaprime.com.br ' }
+
+  ]
 
   constructor() { }
 
